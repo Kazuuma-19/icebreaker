87df32b5f72f49ba1bdb39dd62a5e5579aa14a6a
Kazuma Saito <kazu15870@gmail.com>
2023-05-16 19:42:15 +0900

    データベースをセットアップ

---
+++ resources/js/pages/Index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1 class="heading">index test</h1>
+        <Link href="/show">Change page</Link>
+    </div>
+</template>
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
---
+++ resources/js/pages/Show.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="hello">
+        <h1 class="show">show page</h1>
+        <Link href="/">Change page</Link>
+    </div>
+</template>
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<style lang="scss">
+@import "../../sass/main.scss";
+</style>
