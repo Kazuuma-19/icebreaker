cf04d7f711a1f00092f51171202528687a327bbf
Kazuma Saito <kazu15870@gmail.com>
2023-07-09 15:34:04 +0900

    fix: #9 シンボリックリンクをtopに変更

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import viteImagemin from 'vite-plugin-imagemin';
 import path from 'path';
 
 export default defineConfig({
-    base: '/icebreaker/',
+    base: '/top/',
     plugins: [
         laravel({
             input: ['resources/sass/style.scss', 'resources/js/app.js'],
